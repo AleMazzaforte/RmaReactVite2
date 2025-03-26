c447a9b938cc7768817f0856af08625db820fea3
AleMazzaforte <109003400+AleMazzaforte@users.noreply.github.com>
2025-03-26 01:06:07 -0300

    ultima prueba

--- reactvitefront/vite.config.js
+++ reactvitefront/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   plugins: [    
     react(),
@@ -10,7 +11,7 @@ export default defineConfig({
     postcss: './postcss.config.mjs',
   },
   build: {
-    cssCodeSplit: false,
+    outDir: 'dist',
   }
 })
 
